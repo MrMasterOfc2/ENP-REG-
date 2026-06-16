f9948e03098592a3de3adb20c58d8b7dedadbb0d
Sahan Maduwantha <madumax2006@gmail.com>
2026-06-16 14:57:00 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,10 @@
 window.ENP_FIREBASE_CONFIG={
-  enabled:false,
-  apiKey:"",
-  authDomain:"",
-  databaseURL:"",
-  projectId:"",
-  appId:""
+  enabled:true,
+  apiKey:"AIzaSyA1n8wRePqEdAiU3_VfqSFvYJ_k4Gfcp2U",
+  authDomain:"irosh-entertainment.firebaseapp.com",
+  databaseURL:"https://irosh-entertainment-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId:"irosh-entertainment",
+  storageBucket:"irosh-entertainment.appspot.com",
+  messagingSenderId:"308550605993",
+  appId:"1:308550605993:web:a732a3741542b883e61db8"
 };
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="enp-student-portal-v11";
+const CACHE_NAME="enp-student-portal-v12";
 const APP_SHELL=[
   "./",
   "./index.html",
